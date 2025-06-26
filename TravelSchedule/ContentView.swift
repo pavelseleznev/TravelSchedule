c178f44487631b14000c409e8cd7be7986697c19
PavelSeleznev <seleznevpm@icloud.com>
2025-06-26 08:58:21 +0300

    Initial Commit

---
+++ TravelSchedule/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TravelSchedule
+//
+//  Created by Pavel Seleznev on 6/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TravelSchedule/TravelScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleApp.swift
+//  TravelSchedule
+//
+//  Created by Pavel Seleznev on 6/26/25.
+//
+
+import SwiftUI
+
+@main
+struct TravelScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
